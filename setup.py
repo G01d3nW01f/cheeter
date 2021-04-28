7865ee9ba4e86e0a337df087e2ef4f2ca54eae76
Ki11i0n4ir3 <75846902+Ki11i0n4ir3@users.noreply.github.com>
2021-04-28 17:13:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ if os.getuid() != 0:
   print("[!]You must execute as root, say the magic word")
   sys.exit()
 
+os.system("mkdir ~/cheatsheets")
+os.system("cp cheatsheet.txt ~/chatsheets/")
 os.system("chmod +x cheeter.py")
 os.system("cp cheeter.py /usr/local/bin/cheeter")
 
