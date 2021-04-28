0fa05f3e28d3834f834f636c15e06dc1955452be
Ki11i0n4ir3 <75846902+Ki11i0n4ir3@users.noreply.github.com>
2021-04-28 17:12:15 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+if os.getuid() != 0:
+  print("[!]You must execute as root, say the magic word")
+  sys.exit()
+
+os.system("chmod +x cheeter.py")
+os.system("cp cheeter.py /usr/local/bin/cheeter")
+
+print("Done.........")
